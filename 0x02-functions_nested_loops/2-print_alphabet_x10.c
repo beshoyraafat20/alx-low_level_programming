1b22c584696b4afce4f90adfdb059d44344d1671
beshoyraafat20 <beshoyraafat20@gmail.com>
2022-12-13 22:34:59 +0200

    10 times alphabet lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_alphabet_x10 - 10 times the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success).
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	int n;
+
+	for (i = 0; i <= 10 ; i++)
+	{
+		for (n = 'a'; n <= 'z'; n++)
+		{
+			_putchar(n);
+		}
+		_putchar('\n');
+	}
+}
