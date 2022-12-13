690bc273f0db54f4164b7fdcfff7b9a5a65db299
beshoyraafat20 <beshoyraafat20@gmail.com>
2022-12-14 01:07:15 +0200

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+# include "main.h"
+# include <stdio.h>
+# include <cmath>
+
+/**
+ * _abs - computes the absolute value of an integer.
+ *
+ * @c: character in ASCII code.
+ *
+ * Return: x
+ *
+ */
+
+int _abs(int c)
+{
+	int x;
+
+	x = abs(c);
+
+	return (x);
+
+}
