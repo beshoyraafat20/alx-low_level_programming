6a31a6ffbfe62b8931bf70dfa3a38273d106ea38
beshoyraafat20 <beshoyraafat20@gmail.com>
2022-12-13 18:19:23 +0200

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,3 +1,4 @@
+# include "main.h"
 # include <stdio.h>
 
 /**
@@ -8,7 +9,17 @@
  * Return: Always 0.
  */
 
-int _putchar(char c)
+int main(void)
 {
-	return (write(1, &c, 1);
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
+
+	return (0);
 }
