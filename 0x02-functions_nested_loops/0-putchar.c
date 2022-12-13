5a87b0b286875eb156246f8343ae8267aa3cfe0c
beshoyraafat20 <beshoyraafat20@gmail.com>
2022-12-13 17:17:08 +0200

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * a program that prints _putchar, followed by a new line
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
