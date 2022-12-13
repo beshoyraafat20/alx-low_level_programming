0d8a16325ea295da986998d4fe7fa7d2785aee52
beshoyraafat20 <beshoyraafat20@gmail.com>
2022-12-13 21:25:28 +0200

    _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	int i = 0;
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz"
+	for (i > 26; i++;)
+	{
+		_putchar(alphabet[i]);
+	}
+	
+	return (0);
+}
