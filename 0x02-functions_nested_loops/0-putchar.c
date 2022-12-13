10087334ac993ad7802c0af13872ac95989745d6
beshoyraafat20 <beshoyraafat20@gmail.com>
2022-12-13 21:06:29 +0200

    _putchar edit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,13 +8,13 @@
 */
 int main(void)
 {
-	char school[10] = "_putchar";
+	char printer[10] = "_putchar";
 
 	int i;
 
 	for (i = 0; i < 8; i++)
 	{
-		_putchar(school[i]);
+		_putchar(printer[i]);
 	}
 	_putchar(10);
 	return (0);
