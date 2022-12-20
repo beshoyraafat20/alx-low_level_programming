bdf4fe929eafb43dd67bf86c40abf431277361f7
beshoyraafat20 <beshoyraafat20@gmail.com>
2022-12-20 03:47:19 +0200

    4-p

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,5 +5,16 @@
 
 void print_rev(char *s)
 {
-	printf("%s",strrev(s));
+	len = strlen(*s);
+	left = 0;
+	right = len - 1;
+
+	for (i = left; i <right; i++)  
+	{  
+        	temp = *s[i];  
+		*s[i] = *s[right];  
+		*s[right] = temp;  
+		right--;  
+	}  
+    printf ("%s",  str);
 }
