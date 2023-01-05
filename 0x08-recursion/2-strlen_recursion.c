00db8c0e176207bc262dfae1d3cc870313742d1e
beshoyraafat20 <beshoyraafat20@gmail.com>
2023-01-05 04:34:54 +0200

    2-strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * _strlen_recursion - calculates the length of a string using recursion
+ * @s: pointer to string to be measured
+ *
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+        	return 0;
+	}
+	else
+	{
+		return 1 + _strlen_recursion(s + 1);
+	}
+}
