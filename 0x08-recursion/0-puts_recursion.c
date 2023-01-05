f7956e24741f170163fe93eef9d152528625ccef
beshoyraafat20 <beshoyraafat20@gmail.com>
2023-01-05 04:28:03 +0200

    -puts

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,27 +7,14 @@
  */
 void _puts_recursion(char *s)
 {
-    // Base case: end of string reached
-    if (*s == '\0')
-    {
-        // Print new line
-        putchar('\n');
-        return;
-    }
-
-    // Recursive case: end of string not reached
-    else
-    {
-        // Print current character
-        putchar(*s);
-
-        // Move to next character in string and recursively call _puts_recursion()
-        _puts_recursion(s + 1);
-    }
-}
-
-int main(void)
-{
-    _puts_recursion("Hello, world!");
-    return 0;
+	if (*s == '\0')
+	{
+		putchar('\n');
+		return;
+	}
+	else
+	{
+		putchar(*s);
+		_puts_recursion(s + 1);
+	}
 }
